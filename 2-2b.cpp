572947ddf68683c6cb986acefe8ac4bbcc7784ee
abirislam <akumareign@gmail.com>
2024-04-19 23:09:13 -0400

    2-2b

---
+++ 2-2b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int getValueFromUser() {
+    std::cout << "Enter a value: ";
+    int input{};
+    std::cin >> input;
+    return input;
+}
+
+int main() {
+    int x{getValueFromUser()};
+    int y{getValueFromUser()};
+    std::cout << x << " + " << y << " = " << x + y << '\n';
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ CPPFLAGS = -std=c++20 -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Werr
 # 1-x.o: 1-x.cpp
 # 	g++ $(CPPFLAGS) -o 1-x 1-x.cpp
 
-2-2.o: 2-2.cpp
-	g++ $(CPPFLAGS) -o 2-2 2-2.cpp
+2-2b.o: 2-2b.cpp
+	g++ $(CPPFLAGS) -o 2-2b 2-2b.cpp
 
 clean:
 	rm *.o output
