5bd893b4376490273c1a5ca0b98e8e836c15fbea
abirislam <akumareign@gmail.com>
2024-05-01 23:54:41 -0400

    if else added into the conditional

--- ch4/4-10.cpp
+++ ch4/4-10.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Please enter the number 1:\t";
+    std::cout << "Please enter the number 0 or 1:\t";
     int x{};
     std::cin >> x;
 
     if (x == 1)
-        std::cout << "Thanks for entering the correct number!\n";
+        std::cout << "Thanks for entering the true number!\n";
+    else if (x == 0)
+        std::cout << "Thanks for entering the false number!\n";
     else
         std::cout << "Wow, you failed!\n";
 
