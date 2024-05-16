30a1e43b4175c5bdb84bba8458340444cf7a8bdf
abirislam <akumareign@gmail.com>
2024-05-16 18:25:45 -0400

    7-13 revised

--- ch7/7-13.cpp
+++ ch7/7-13.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+
+// inline namespaces allow us to test new functions that will replace old ones
+// inlines will get favored over regular namespaces
+// so the play is to put the old function inside a namespace
+// put the new function inside a namespace as well but make sure the new one is inline
+// so you can test it
+
+// also there are unnamed namespaces
+// these namespaces get internal linkage, much like static functions
+// but u can group all functions u want to make static in one section with an unnamed 
+// namespace to make readability better
+
 namespace V1 
 {
     void print()
@@ -16,6 +28,27 @@ inline namespace V2
     }
 }
 
+// combination of unnamed and inline namespace
+inline namespace V3
+{
+    namespace 
+    {
+        void print2()
+        {
+            std::cout << "this is unammed inline namespace\n";
+        }
+    }
+}
+
+// unnamed namespace (to get internal linkage)
+namespace
+{
+    void bob()
+    {
+        std::cout << "doke!\n";
+    }
+}
+
 int main() 
 {
     using std::cout;
@@ -24,5 +57,7 @@ int main()
     print();
     cout << "now calling v1 explicitly\n";
     V1::print();
+    V3::print2();
+    bob();
     return 0;
 }
