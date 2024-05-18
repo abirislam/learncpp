4d0f4e1333549fb9eaf834525d04f2c7d6467638
abirislam <akumareign@gmail.com>
2024-05-17 20:58:28 -0400

    8-8q4

---
+++ ch8/8-8q4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int outer{ 1 };
+    while (outer <= 5)
+    {
+        int inner{ 5 };
+        while (inner > outer)
+        {
+            std::cout << "  ";
+            --inner;
+        }
+        while (inner <= outer && inner > 0) 
+        {
+            std::cout << inner << ' ';
+            --inner;
+        }
+        std::cout << '\n';
+        ++outer;
+    }
+
+    return 0;
+}
