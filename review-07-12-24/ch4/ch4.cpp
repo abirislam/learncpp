7c20a370d9235feab06ec5e4c46e6076ed3226a3
abirislam <akumareign@gmail.com>
2024-07-12 16:11:45 -0400

    restructure review folder

---
+++ review-07-12-24/ch4/ch4.cpp
@@ -0,0 +1,10 @@
+// create a program that allows users to input any two decimal numbers and perform operations on them
+// functions should be in a different file and a header file should also be made
+// create a makefile
+
+#include <iostream>
+#include "ch4_funcs.h"
+
+int main() {
+
+}
